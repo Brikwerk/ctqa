8561d065e18159c59090214502f6a3adf9b54a19
Brikwerk <reece@brikwerk.com>
2019-07-19 16:04:10 -0700

    Added password saving ability

--- ctqa/encryption.py
+++ ctqa/encryption.py
@@ -6,6 +6,11 @@ Encryption functions
 from cryptography.fernet import Fernet
 import os, sys
 
+# Logging
+import logging
+from ctqa import logutil
+logger = logging.getLogger(logutil.MAIN_LOG_NAME)
+
 LOCATION = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 
@@ -17,12 +22,33 @@ def get_fernet_key():
     key = Fernet.generate_key()
     with open(keypath, 'wb') as outfile:
       outfile.write(key)
-      outfile.close()
   else:
     with open(keypath, 'rb') as infile:
       key = infile.read()
-      infile.close()
 
   # Create a Fernet key from the base key
   return Fernet(key)
-  del(key)
+
+
+def save_password(password):
+  password_encoded = password.encode()
+  f = get_fernet_key()
+  encrypted = f.encrypt(password_encoded)
+  datapath = os.path.join(LOCATION, 'data', 'enc')
+  with open(datapath, 'wb') as outfile:
+    outfile.write(encrypted)
+
+
+def get_password():
+  f = get_fernet_key()
+  datapath = os.path.join(LOCATION, 'data', 'enc')
+  password = ""
+  try:
+    with open(datapath, 'rb') as infile:
+      encoded_password = infile.read()
+      password = f.decrypt(encoded_password).decode("utf-8")
+  except Exception as e:
+    logger.error(str(e))
+  
+  return password
+  
