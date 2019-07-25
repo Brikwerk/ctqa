6d70fc37e97571a6c560557be12a8c3c53bffd94
Brikwerk <reece@brikwerk.com>
2019-07-25 10:33:57 -0700

    Update documentation for new version
    Updated to v1.0
    
    Updated version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Reece Walsh'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
